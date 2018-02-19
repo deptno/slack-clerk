20daa1810183baa61ad9c757a81e949a50fb73c3
이봉균 <bglee@rsquare.co.kr>
2018-02-19 21:05:01 +0900

    [WIP] setup dynamodb

---
+++ __tests__/dynamodb.spec.ts
@@ -0,0 +1,44 @@
+import {DynamoDB} from 'aws-sdk'
+
+describe('dynamodb', () => {
+  const ddb = new DynamoDB({
+    region  : 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  let tableName
+  it('list tables', async done => {
+    ddb.listTables((err, data) => {
+      expect(err).toBeNull()
+      tableName = data.TableNames[0]
+      done()
+    })
+  })
+
+  it('describe table', async done => {
+    console.log('tableName', tableName)
+    ddb.describeTable({
+      TableName: tableName
+    }, (err, data) => {
+      expect(err).toBeNull()
+      done()
+    })
+  })
+})
+
+describe('dynamodb client', () => {
+  const docClient = new DynamoDB.DocumentClient({
+    region  : 'localhost',
+    endpoint: 'http://localhost:8000'
+  })
+  it('get', done => {
+    docClient.get({
+      TableName: 'slack-clerk-dev',
+      Key      : {
+        id: 'abc'
+      }
+    }, (err, data) => {
+      expect(err).toBeNull()
+      done()
+    })
+  })
+})

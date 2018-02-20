39f5344807b26a0ef7f24d8765255cbe1c28b0ee
이봉균 <bglee@rsquare.co.kr>
2018-02-20 18:00:39 +0900

    add team

--- route/index.ts
+++ route/index.ts
@@ -1,5 +1,5 @@
 import {APIGatewayEvent, Callback, Context, Handler} from 'aws-lambda'
-import {Event, NewMessageEvent, UrlVerificationPacket} from '@deptno/slack'
+import {Event, MessagePacket, NewMessageEvent, UrlVerificationPacket} from '@deptno/slack'
 import * as getUrl from 'get-urls'
 import {putItem} from '../lib/aws/dynamodb-client'
 
@@ -30,14 +30,19 @@ function route(packet) {
       case 'url_verification':
         return handleUrlVerification(packet)
       case 'event_callback':
-        return handleEventCallback(packet.event)
+        return handleEventCallback(packet)
       default:
         return null
     }
   } catch (ex) {
     if (packet.message === 'unknown_event') {
+      console.log('unknown_event', packet)
+
       //warning: infinity loop
-      return route(packet.event)
+      return route({
+        ...packet,
+        ...packet.event
+      })
     }
     return null
   }
@@ -46,26 +51,29 @@ function handleUrlVerification(event: UrlVerificationPacket) {
   return event.challenge
 }
 
-function handleEventCallback(event: Event) {
-  switch (event.type) {
+function handleEventCallback(packet: MessagePacket) {
+  const {team_id: team, event} = packet
+  const {type, channel} = event
+
+  switch (type) {
     case 'message':
-      return handleMessage(event.channel, event)
+      return handleMessage(team, channel, event)
   }
 }
 
-function handleMessage(channel, event: Event) {
+function handleMessage(team, channel, event: Event) {
   if ('subtype' in event) {
     if (event.subtype !== 'message_changed') {
       throw new Error('unknown message')
     }
-    return handleMessage(channel, event.message as any as NewMessageEvent)
+    return handleMessage(team, channel, event.message as any as NewMessageEvent)
   }
-  return handleNewMessage(channel, event)
+  return handleNewMessage(team, channel, event)
 }
 
-function handleNewMessage(channel, event: NewMessageEvent) {
+function handleNewMessage(team, channel, event: NewMessageEvent) {
   const {user, text, ts} = event
-  const urls: Set<string> = getUrl(event.text)
+  const urls: Set<string> = getUrl(text)
 
   if (urls.size > 0) {
     //todo: [recommended] It can be replace with batchWrite
@@ -74,7 +82,7 @@ function handleNewMessage(channel, event: NewMessageEvent) {
     urls.forEach(async url => {
       try {
         const timestamp = parseFloat(ts.split('.')[0])
-        await putItem({url, user, channel, timestamp})
+        await putItem({url, user, teamId: team, channel, timestamp})
       } catch (ex) {
         console.error(ex)
         console.log('ignored', event)
